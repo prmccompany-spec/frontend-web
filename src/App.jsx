d91bdf6f93051795c86930257895f2da26a235bc
prmccompany-spec <prmccompany@gmail.com>
2026-04-13 14:51:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import AppRoutes from './routes/AppRoutes';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import './App.css';
-
+ 
 function App() {
   return (
     <Router>
